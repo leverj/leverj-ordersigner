e5d7c74d99d9577be99130a3093bca14ac3b059a
Shashank Tiwari <tshanky@gmail.com>
2019-10-29 11:01:52 -0700

    adjust for quote to base ratio

--- leverj_ordersigner/signer.py
+++ leverj_ordersigner/signer.py
@@ -42,11 +42,9 @@ def _get_evm_parameters(order, order_instrument, signer):
 
     # price
     abi_types.append('uint256')
-    price = _convert_to_unit_lowest_denomination(
-        order['price'], order_instrument['quote']['decimals'])
-    price_adjusted_for_base_to_quote_ratio = _adjust_for_base_to_quote_ratio(
-        price, order_instrument['base']['decimals'], order_instrument['quote']['decimals'])
-    evm_parameters.append(price_adjusted_for_base_to_quote_ratio)
+    price_adjusted_for_quote_to_base_ratio = _adjust_for_quote_to_base_ratio(
+        order['price'], order_instrument['base']['decimals'], order_instrument['quote']['decimals'])
+    evm_parameters.append(price_adjusted_for_quote_to_base_ratio)
 
     # base
     abi_types.append('address')
@@ -77,8 +75,8 @@ def _get_side_as_int(side):
     return side_dict[side]
 
 
-def _adjust_for_base_to_quote_ratio(price, base_decimals, quote_decimals):
-    adjustment_power_factor = base_decimals - quote_decimals
+def _adjust_for_quote_to_base_ratio(price, base_decimals, quote_decimals):
+    adjustment_power_factor = quote_decimals - base_decimals
     adjusted_price = int(price * pow(10, adjustment_power_factor))
     return adjusted_price
 
